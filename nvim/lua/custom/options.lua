aa11943a48999f2a87681b93b24d475183fe90e1
HAM-AFK <herschel.menezes@forcepoint.com>
2024-10-19 12:35:47 +0530

    Updated Nvim Config files

---
+++ nvim/lua/custom/options.lua
@@ -0,0 +1,15 @@
+local M = {}
+
+function M.setup()
+	-- Add your custom options here
+	-- For example:
+	-- vim.opt.scrolloff = 8
+	vim.opt_global.numberwidth = 2 -- set number column width to 2 {default 4}
+	vim.opt_global.shiftwidth = 4 -- the number of spaces inserted for each indentation
+	vim.opt_global.tabstop = 4  -- insert n spaces for a tab
+	vim.opt_global.softtabstop = 4 -- Number of spaces that a tab counts for while performing editing operations
+	vim.opt.undolevels = 2000
+	vim.o.winbar = "%=%{v:lua.require'nvim-navic'.get_location({ highlight = true, icons = true })}"
+end
+
+return M
